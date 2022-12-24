1e7859e27499cd4ac80dfce94ba05a0e19875bc9
«Асхат <askhat.akylbek.06@bk.ru>
2022-12-24 20:43:20 +0600

    Lesson 05

---
+++ 51 home work/Program.cs
@@ -0,0 +1,19 @@
+﻿// С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+int N=Convert.ToInt32(Console.ReadLine());
+int[] t;
+t= new int[N];
+for (int i=0;i<N;i++)
+     t[i]=Convert.ToInt32(Console.ReadLine());
+
+int Solve(int[] t)
+{   
+    int count=0;
+    foreach(int el in t)
+    {
+        if (el>0)
+            count++;       
+    }
+    return count;
+
+}
+System.Console.WriteLine(Solve(t));
---
+++ 56 home work/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу копирования массива
+
+int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+int[] a=RandomIntArray(5,0,5);
+Print(a);
+int[] copy = a.Take(a.Length).ToArray();
+System.Console.WriteLine();
+ Console.WriteLine(String.Join(" ",copy)); 
