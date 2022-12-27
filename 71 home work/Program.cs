592c3eb48b4dcd87d0b79bba409dc896cd5530cb
«Асхат <askhat.akylbek.06@bk.ru>
2022-12-27 21:18:48 +0600

    Lesson 07

---
+++ 71 home work/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void Loop(int N)
+{
+    if (N > 0)
+    {
+        System.Console.WriteLine(N);
+        Loop(N-1);
+    }    
+}
+
+
+Loop(10);
---
+++ 75 home work/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления функции Аккермана - 5*
+static int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ 
+Console.WriteLine(Akk(1, 2)); // 4
+Console.WriteLine(Akk(0, 0)); // 1
+Console.WriteLine(Akk(4, 5)); // Stack Overflow !
+ 
+Console.ReadKey();
